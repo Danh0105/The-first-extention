1fd726ef83f4febe96d7339aebd8f4df60b0b408
Danh0105 <150211320+Danh0105@users.noreply.github.com>
2024-06-20 14:47:04 +0700

    Update main.ts, test.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
-basic.forever(function () {
-	
-})
+namespace screenMagic{
+    export function lineIndex(index:number):void{
+    const x = Math.round(index/5);
+    const y = Math.round(index%5);  
+    led.plot(x,y);
+    }
+}
