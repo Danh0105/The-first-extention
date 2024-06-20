bae42ea6bc1c4eea18df485335784cef1bab61d1
Danh0105 <150211320+Danh0105@users.noreply.github.com>
2024-06-20 14:52:31 +0700

    Update main.blocks, main.ts, test.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,14 @@
 namespace screenMagic{
-    export function lineIndex(index:number):void{
-    const x = Math.round(index/5);
-    const y = Math.round(index%5);  
+    /**
+     * Address LED linearly row first
+     * %blockId=screenmagicplotat
+     * %block="Plot at index"
+     * %index.min=0 index.max=25
+     */
+    export function plotAt(index:number):void{
+    index|=0;
+    const x = Math.floor(index/5);
+    const y = Math.floor(index%5);
     led.plot(x,y);
     }
 }
